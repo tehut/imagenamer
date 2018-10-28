220d6977dc44e6489042581cec7d660a8f52db91
tamikot <tamikodevo@gmail.com>
2018-10-27 17:16:26 -0700

    add get request
    Signed-off-by: tamikot <tamikodevo@gmail.com>

--- site/index.js
+++ site/index.js
@@ -6,3 +6,8 @@ $( document ).ready(function() {
     console.log( "ready!" );
   });
 });
+
+$.get( "https://wa797zuhxf.execute-api.us-east-1.amazonaws.com/prod/helloworld", function( data ) {
+  console.log(data);
+  alert( data );
+});
