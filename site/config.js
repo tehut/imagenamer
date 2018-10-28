8c680aa574a113474d427e4f0b1858cf4ce2022c
tamikot <tamikodevo@gmail.com>
2018-10-27 17:07:45 -0700

    helloworld endpoint working
    Signed-off-by: tamikot <tamikodevo@gmail.com>

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
-window._config = {
+export default window._config = {
   api: {
-    invokeUrl: "https://wa797zuhxf.execute-api.us-east-1.amazonaws.com/prod"
+    invokeUrl: "https://wa797zuhxf.execute-api.us-east-1.amazonaws.com/prod/helloworld"
   }
 }
--- site/index.js
+++ site/index.js
@@ -1,3 +1,6 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.js');
+
 $( document ).ready(function() {
   $( ".buttonchan" ).on("click", function () {
     console.log( "ready!" );
