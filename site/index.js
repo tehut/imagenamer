44d3779b807488070a1c0cbee08e1de606873efd
tamikot <tamikodevo@gmail.com>
2018-10-27 21:22:56 -0700

    add api gateway shiiiiiit
    Signed-off-by: tamikot <tamikodevo@gmail.com>

--- site/index.js
+++ site/index.js
@@ -1,5 +1,6 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.js');
+var apigateway = new AWS.APIGateway();
 
 $( document ).ready(function() {
   $( ".buttonchan" ).on("click", function () {
@@ -7,7 +8,11 @@ $( document ).ready(function() {
   });
 });
 
-$.get( "https://wa797zuhxf.execute-api.us-east-1.amazonaws.com/prod/helloworld", function( data ) {
-  console.log(data);
-  alert( data );
-});
+var params = {
+  "input": "{\"comments\": \"test\"}",
+  "stateMachineArn": "arn:aws:states:us-east-1:459472629219:stateMachine:StepTest-the-second"
+}
+apigateway.createApi(params, function (err, data) {
+  if (err) console.log(err)
+  console.log(data)
+})
